fed0face2a28fd9219719145b6ce12a14ec01209
DanielStefan11 <dannielstefan592@gmail.com>
2022-02-12 00:38:41 +0200

    page style & vault filters

--- src/Pages/Vault/Vault.tsx
+++ src/Pages/Vault/Vault.tsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Vault.module.scss";
+import { FaSortAlphaDown as AtoZIcon, FaSortAlphaDownAlt as ZtoAIcon } from "react-icons/fa";
+
+interface SortIcons {
+   AtoZ: boolean;
+   ZtoA: boolean;
+}
 
 const Vault: React.FC = (): JSX.Element => {
+   const [sortIcons, setSortIcons] = useState<SortIcons>(() => {
+      return {
+         AtoZ: true,
+         ZtoA: false,
+      };
+   });
+
+   const handleSortIcons = (iconId: string) => {
+      switch (iconId) {
+         case "a-z":
+            setSortIcons({ AtoZ: true, ZtoA: false });
+            break;
+
+         case "z-a":
+            setSortIcons({ AtoZ: false, ZtoA: true });
+            break;
+
+         default:
+            setSortIcons({ AtoZ: true, ZtoA: false });
+      }
+   };
+
    return (
-      <div>
-         <h1>vault</h1>
+      <div className={`page`}>
+         <div className={styles.filtersContainer}>
+            <input type="text" className={styles.search} placeholder="Search..." />
+
+            <div className={styles.sortContainer}>
+               <AtoZIcon
+                  className={`${styles.sortIcons} ${sortIcons.AtoZ && styles.activeSortIcon}`}
+                  onClick={() => handleSortIcons("a-z")}
+               />
+
+               <ZtoAIcon
+                  className={`${styles.sortIcons} ${sortIcons.ZtoA && styles.activeSortIcon}`}
+                  onClick={() => handleSortIcons("z-a")}
+               />
+            </div>
+         </div>
       </div>
    );
 };
