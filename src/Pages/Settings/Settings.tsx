3fa012ea5ed09055d0129ba7ec965e14baff31c4
DanielStefan11 <dannielstefan592@gmail.com>
2022-03-27 16:38:50 +0300

    refactored dark mode card in settings

--- src/Pages/Settings/Settings.tsx
+++ src/Pages/Settings/Settings.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import styles from "./Settings.module.scss";
+import { Row, Col } from "react-bootstrap";
+import DarkMode from "../../Components/SettingsComponents/DarkMode/DarkMode";
 import PageHeading from "../../Components/PageHeading/PageHeading";
-import ToggleButton from "../../Components/ToggleButton/ToggleButton";
 import { useDarkModeContext } from "../../Context/DarkModeProvider";
 
 const Settings: React.FC = (): JSX.Element => {
@@ -12,19 +12,11 @@ const Settings: React.FC = (): JSX.Element => {
       <div className={`page darkModeTransition ${darkModeContext?.darkMode ? "backgroundDarkMode" : "pageLightMode"}`}>
          <PageHeading />
 
-         <div className={`mt-5 d-flex flex-wrap ${styles.settingsContainer}`}>
-            <div
-               className={`shadow darkModeTransition ${styles.box} ${
-                  darkModeContext?.darkMode ? "elementBgDarkMode" : styles.boxLightMode
-               }`}
-            >
-               <h4 className="text-center mb-4 size-26 weight-700">Enable dark mode</h4>
-
-               <div className="w-100 d-flex justify-content-center">
-                  <ToggleButton checkState={darkModeContext?.darkMode} toggle={darkModeContext!.handleToggleDarkMode} />
-               </div>
-            </div>
-         </div>
+         <Row xs={1} sm={2} md={3} className={`mt-5`}>
+            <Col>
+               <DarkMode />
+            </Col>
+         </Row>
       </div>
    );
 };
